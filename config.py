5e9fd7386673ea38d78992b85669fb18b2eae79f
TelMTProto <117292475+TelMTProto@users.noreply.github.com>
2022-12-05 10:36:28 -0800

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ MODES = {
 TLS_DOMAIN = "www.irancell.io"
 
 # Tag for advertising, obtainable from @MTProxybot
-AD_TAG = "c9f96504e1e6ffe934f134baf7ca5255"
+AD_TAG = "cdbd3e12bc2ef22bb100638c0614e2ac"
 
 #7gAAAAAAAAAAAAAAAAAAAAB3d3cuaXJhbmNlbGwuaW8
 #curl -o MTProo.sh -L https://git.io/fjo3u && bash MTProo.sh --port 443 --secret 00000000000000000000000000000000 --tag 8bd7c59cd625d7991b86a77dfbc5fcff --tls www.HideProxi.io --disable-updater
